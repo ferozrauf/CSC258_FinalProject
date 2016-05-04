a45eb2aa31549adebceefa85b3b8d602e3960b2e
ferozrauf <feroz.rauf@smartfinancellc.net>
2016-05-04 05:31:27 -0400

    added heapsort

--- nowsort.c
+++ nowsort.c
@@ -153,11 +153,45 @@ void quickSort( int* arr, int l, int r)
    }
 	
 }
-
+void heapsort(int* arr, unsigned int N) 
+{
+    if(N==0) 
+      return;
+    int t; 
+    unsigned int n = N, parent = N/2, index, child; 
+    while (1) { 
+        if (parent > 0) { 
+            t = arr[--parent]; 
+        } else {
+            n--;              
+            if (n == 0) {
+                return; /
+            }
+            t = arr[n];        
+            arr[n] = arr[0];  
+        }
+        index = parent; 
+        child = index * 2 + 1; 
+        while (child < n) {
+            if (child + 1 < n  &&  arr[child + 1] > arr[child]) {
+                child++;
+            }
+            if (arr[child] > t) {
+                arr[index] = arr[child];
+                index = child; 
+                child = index * 2 + 1;
+            } else {
+                break; 
+            }
+        }
+        arr[index] = t; 
+    }
+}
 
 void sort(int * arr)
 {
 	quickSort(arr,0,BUCKET_SIZE);
+	heapsort(arr,BUCKET_SIZE);
 }
 void sortNumbers(int proc_id)
 {
